ed9b001fd117fe63a452cf0427afd7463238cb00
paplopikasso6-cyber <paplopikasso6@gmail.com>
2026-04-10 05:50:30 +0100

    connect frontend to backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/api/login", {
+      const res = await axios.post("https://nouvelair-backend.onrender.com/api/login", {
         username, password
       });
       if (res.data.success) {
