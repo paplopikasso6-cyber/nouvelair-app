042fdb2b283ac5d050a1341c7b8973511d83486d
paplopikasso6-cyber <paplopikasso6@gmail.com>
2026-04-16 22:48:46 +0100

    fix home import case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import FlightPlanning from "./pages/FlightPlanning";
 import AOG from "./pages/AOG";
 
